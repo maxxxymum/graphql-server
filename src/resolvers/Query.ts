a8850436cc19645e467906581176a3fd1676d678
Max Martyn <martynmaksim.w@gmail.com>
2020-03-17 20:05:53 +0200

    Implement sorting

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,11 @@
 import { GraphQLContext } from '../utils';
-
+import { LinkOrderByInput } from '../generated/prisma-client';
+ 
 export async function feed(_: any, args: {
   filter: string,
   skip: number,
-  first: number
+  first: number,
+  orderBy: LinkOrderByInput
 }, context: GraphQLContext,) {
     const where = args.filter ? {
       OR: [
@@ -15,7 +17,8 @@ export async function feed(_: any, args: {
     const links = await context.prisma.links({
       where,
       skip: args.skip,
-      first: args.first
+      first: args.first,
+      orderBy: args.orderBy
     });
     
     return links;
