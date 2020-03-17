acc184792e72375b52c6ee348c0800eef00b12ae
Max Martyn <martynmaksim.w@gmail.com>
2020-03-17 19:53:33 +0200

    Implement pagination

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,10 @@
 import { GraphQLContext } from '../utils';
 
-export async function feed(_: any, args: {filter: string}, context: GraphQLContext,) {
+export async function feed(_: any, args: {
+  filter: string,
+  skip: number,
+  first: number
+}, context: GraphQLContext,) {
     const where = args.filter ? {
       OR: [
         { description_contains: args.filter },
@@ -8,7 +12,11 @@ export async function feed(_: any, args: {filter: string}, context: GraphQLConte
       ],
     } : {}
   
-    const links = await context.prisma.links({where});
+    const links = await context.prisma.links({
+      where,
+      skip: args.skip,
+      first: args.first
+    });
     
     return links;
 }
